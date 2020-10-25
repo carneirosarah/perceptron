2bef8a57b276ad26f869214be6311bd45ac3b372
carneirosarah <sarahrlcarneiro@gmail.com>
2020-10-25 19:44:37 -0300

    melhorias

--- Perceptron_Gaussianas.py
+++ Perceptron_Gaussianas.py
@@ -26,6 +26,8 @@ class Perceptron:
             for i in range(self.numberOfTimes):
                 
                 O = self.predict(X)
+
+                # W = W + lambda * X * (T - O)
                 self.W += self.learningRate * np.dot(X.T, (T - O)) 
             
         # treinamento sequencial
@@ -39,9 +41,7 @@ class Perceptron:
 
                     # w_ij = w_ij + lambda * x_ki * (t_kj - o_kj)
                     self.W += self.learningRate * np.dot(x_k[np.newaxis].T, (T[k,:] - O[k,:])[np.newaxis])
-            
-        print('erro', T - O)
-             
+                         
     # f(XW - b)
     def predict (self, X):
 
@@ -75,16 +75,14 @@ def main():
         # desvio padrao de cada gaussiana
         sd = float(input('Insira o valor do desvio padrão da {}° gaussiana: '.format(i+1)))
         
-        # padroes
+        # padroes de entrada
         if (i == 0):
             X = np.random.normal(average, sd, (N, 2))
         else:
             X = np.concatenate((np.random.normal(average, sd, (N, 2)), X), axis=0)
     
-    # adiciona o bias aos padroes
+    # adiciona o bias aos padroes de entrada
     X = np.concatenate((np.full((3*N, 1), -1.0), X), axis=1)
-
-    print(X)
 
     # cria os conjuntos de treinamento e de teste
     rand_state = np.random.RandomState(0)
@@ -93,14 +91,10 @@ def main():
     trainSet = X[0:idx, :]
     testSet = X[idx+1:len(X), :]
 
-    print('train', trainSet)
-    print('test', testSet)
-
     # alvos
     T = np.zeros((N,2))
     T = np.concatenate((T, np.concatenate((np.zeros((N,1)), np.ones((N,1))), axis=1)), axis=0)
     T = np.concatenate((T, np.concatenate((np.ones((N,1)), np.ones((N,1))), axis=1)), axis=0)
-    
     rand_state.seed(0)
     rand_state.shuffle(T)
     tTrain = T[0:idx, :]
@@ -110,11 +104,9 @@ def main():
     # pesos sinapticos
     weights = np.random.normal(0, 0.01, (3, 2))
 
-    print(weights)
-
     p = Perceptron (learningRate= learningRate, numberOfTimes= numberOfTimes, weights = weights)
 
-    # plota os padroes de entrada por classe (conjunto de teste)
+    # plota os padroes de entrada por classe (conjunto de treinamento)
     fig = plt.figure()
     plt.scatter(trainSet[:,1], trainSet[:,2],marker = 'o', c = (tTrain[:,0] + tTrain[:,1]))
 
@@ -127,11 +119,26 @@ def main():
         x_plot = np.linspace(-4, 4, 50)
         plt.plot(x_plot, (p.W[0][j] - (p.W[1][j] * x_plot))/p.W[2][j], '--', color=colors[j])
     
+    plt.title('Conjunto de treinamento')
     plt.show()
+
+    # plota os padroes de entrada por classe (conjunto de teste)
+    fig = plt.figure()
+    plt.scatter(testSet[:,1], testSet[:,2],marker = 'o', c = (tTest[:,0] + tTest[:,1]))
 
     # avalia o classificador utilizando o conjunto de teste
 
     O = p.testPcn(testSet)
+    print('Acurácia', (1 - np.sum(np.absolute(O-tTest))/len(tTest)) * 100, '%')
+
+    # plota as fronteiras entre as classes
+    colors = ['red', 'black']
+    for j in range(0, 2):
+        x_plot = np.linspace(-4, 4, 50)
+        plt.plot(x_plot, (p.W[0][j] - (p.W[1][j] * x_plot))/p.W[2][j], '--', color=colors[j])
+    
+    plt.title('Conjunto de teste')
+    plt.show()
 
 if __name__ == '__main__':
     main()
